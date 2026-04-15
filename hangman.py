729a555a0ce60d01cad5ce6d095676239ae134cb
Arkatanu Mukherjee <arkatanumukherjee123@gmail.com>
2026-04-15 13:18:13 +0530

    Initial commit: Modular Hangman Game

---
+++ hangman.py
@@ -0,0 +1,69 @@
+import random
+
+import hangman_words
+
+import hangman_art
+
+word_list = hangman_words.word_list
+
+lives = 6
+
+logo = hangman_art.logo
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+
+level = input("Choose Your Level -- Easy of Hard: ").lower()
+if level == "easy":
+    print(chosen_word)
+else:
+    print("Go Ahead and Guess the Word!")
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder +=  "_"
+print("Word to guess " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+    print(f"**************************{lives}/6 LIVES LEFT*************************")
+    guess = input("Guess a letter: ").lower()
+
+    if guess in correct_letters:
+        print(f"You've already guessed {guess}")
+    
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+    if "_" not in display:
+            game_over = True
+            print("***********************You WIN************************")
+
+    if guess not in chosen_word:
+        lives -= 1
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        
+        stages = hangman_art.stages
+
+        print(stages[lives])
+
+        if lives == 0:
+            game_over = True
+
+            print(f"**********************IT WAS {chosen_word}! YOU LOSE************************")
+        
